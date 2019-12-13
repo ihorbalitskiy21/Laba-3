d6f1f8ecb31131075555715a2f1782b0ff0de1e3
ihorbalitskiy21 <55256928+ihorbalitskiy21@users.noreply.github.com>
2019-12-13 16:38:17 +0200

    Add files via upload

---
+++ client.py
@@ -0,0 +1,19 @@
+import socket, threading
+
+
+def read_sok():
+    while True:
+        data = sor.recv(1024)
+        print(data.decode('utf-8'))
+
+
+server = '192.168.1.3',5000  # Данные сервера
+alias = input("Write your name: ") # Вводим наш псевдоним
+sor = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sor.bind(('', 0))# Задаем сокет как клиент
+sor.sendto((alias+' Connect to server').encode('utf-8'), server)# Уведомляем сервер о подключении
+potok = threading.Thread(target=read_sok)
+potok.start()
+while True:
+    mensahe = input(">")
+    sor.sendto(('['+alias+']'+mensahe).encode('utf-8'), server)
